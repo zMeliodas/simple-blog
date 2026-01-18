d11f7920c32dedb2d6b7d8a71b2702d880a62b89
Ced <98132233+zMeliodas@users.noreply.github.com>
2026-01-19 03:51:41 +0800

    fix: responsiveness

--- frontend/src/BlogPage/BlogPage.tsx
+++ frontend/src/BlogPage/BlogPage.tsx
@@ -75,7 +75,7 @@ const BlogPage = () => {
   }
 
   return (
-    <div className="bg-backgroundColor flex flex-col items-center gap-2 pt-24 p-4 h-screen">
+    <div className="bg-backgroundColor flex flex-col items-center gap-2 pt-24 p-4 h-screen overflow-auto">
       <button className="text-indigo-600 font-bold text-3xl pt-4 pb-2">
         Latest Blog Posts
       </button>
